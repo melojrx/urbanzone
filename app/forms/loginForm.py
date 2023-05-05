f9225493fd29fac271ca6abc7da1d38a4f9032c0
Melo Jr <jrmeloafrf@gmal.com>
2023-05-05 00:18:58 -0300

    ajuste tela de login

--- app/forms/loginForm.py
+++ app/forms/loginForm.py
@@ -3,12 +3,12 @@ from wtforms.validators import DataRequired, Email, Length
  
 class LoginForm(Form):
     email = StringField(
-    'Digite seu email',
+    'Digite seu email:',
     validators= [
         Email(message=('Ops. Não nos parece um e-mail válido.'))
     ])
 
-    password = PasswordField('Password', 
+    password = PasswordField('Digite sua senha:', 
     validators = [
         DataRequired(),
         Length(min=8, message='A senha deve ter no mínimo %(min)d caracteres')
