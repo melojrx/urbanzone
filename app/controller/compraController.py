40f1da300ad1f2d7a7f6ff668e5ce0b7d57c2794
Cyro Sabóia <cyrosaboia@gmail.com>
2023-05-17 17:38:23 -0300

    Iniciando compra de ticket

--- app/controller/compraController.py
+++ app/controller/compraController.py
@@ -1,9 +1,14 @@
 import datetime
+import locale
 from flask import flash, redirect, render_template, request, url_for
 from flask_login import current_user, login_required
 
+from app.forms.compraForm import CompraForm
+from app.models.cartaoCreditoModel import CartaoCredito
+
 from ..database import db
 from app.controller.roleRequired import roles_required
+from app.models.ticketModel import Ticket
 from app.models.compraModel import Compra
 from app.rotas.compraRout import compra_bp
 
@@ -12,6 +17,45 @@ class compraController:
 
    global ROWS_PER_PAGE 
    ROWS_PER_PAGE = 10
+
+   @login_required
+   @roles_required('URBANMOB_ADMIN, URBANMOB_GOVERNO')
+   @compra_bp.route('/prepareBuyCard', methods=['GET'])
+   def prepareBuyCard():
+         
+         try:
+
+            form = CompraForm(request.form)
+            locale.setlocale(locale.LC_ALL, 'pt_BR.UTF-8')
+
+            listTicket = Ticket.query.filter( Ticket.datFim.is_(None)).all()
+            listCartaoCredito = CartaoCredito.query.filter(CartaoCredito.datFim.is_(None)).all()
+
+            form.quantidade.choices = [(0, "Selecione...e")]+[(row, str(row)) for row in list(range(1, 11))]
+            form.cartoes.choices = [(0, "Selecione seu cartão")]+[(row.id, row.txtNumero) for row in listCartaoCredito]
+            form.tickets.choices = [(row.id, str(row.txtTicket) + ' - ' +  str(row.horaTicket) + 'H - ' + str(locale.currency(row.valorTicket, grouping=True, symbol=None))) for row in listTicket]
+
+            return render_template('comprarCartao.html', form=form)
+            
+         except Exception as e:
+               flash('Erro: {}'.format(e), 'error') 
+               return render_template('comprarCartao.html', form=form)
+
+   @login_required
+   @roles_required('URBANMOB_ADMIN, URBANMOB_GOVERNO')
+   @compra_bp.route('/buyCard', methods=['GET'])
+   def buyCard():
+         
+         try:
+
+            form = CompraForm(request.form)
+
+ 
+            return render_template('comprarCartao.html')
+            
+         except Exception as e:
+               flash('Erro: {}'.format(e), 'error') 
+               return render_template('comprarCartao.html')
 
    @login_required
    @roles_required('URBANMOB_ADMIN, URBANMOB_GOVERNO')
---
+++ app/forms/compraForm.py
@@ -0,0 +1,29 @@
+from wtforms import Form, SubmitField, SelectField, RadioField
+from wtforms.validators import DataRequired, InputRequired, Length
+
+class CompraForm(Form):
+
+    quantidade = SelectField(
+        'Quantidade:',
+        coerce=int,
+        validators = [
+            DataRequired(message='*Campo Requerido'),
+            InputRequired(message=('*Campo Requerido'))
+    ])
+
+    tickets = RadioField(
+        'Tickets:', 
+        validators = [
+            DataRequired(message='*Campo Requerido'),
+            InputRequired(message=('*Campo Requerido'))
+    ])
+    
+    cartoes = SelectField(
+        'Cartão de Crédito:',
+        coerce=int,
+        validators = [
+            DataRequired(message='*Campo Requerido'),
+            InputRequired(message=('*Campo Requerido'))
+    ]) 
+
+    submit = SubmitField('Estacionar')
